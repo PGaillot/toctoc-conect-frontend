5953e0856b16c1d7ebe6d57a8ef96da26a744013
PGaillot <pr.gaillot@gmail.com>
2024-10-16 18:55:19 +0200

    add door

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DoorComponent } from "./components/door/door.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DoorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
