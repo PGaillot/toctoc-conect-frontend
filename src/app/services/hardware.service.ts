ca58759414bf2932f8c782660f578660bcbcc9d3
PGaillot <pr.gaillot@gmail.com>
2024-10-28 11:55:04 +0100

    udpare hardware service.

--- src/app/services/hardware.service.ts
+++ src/app/services/hardware.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HardwareService {
 
-  constructor() { }
+  constructor(private http:HttpClient) {}
+
+
+  // TODO: résussire a recupere l'id du hardware
+  getHardwareId():Observable<string>{
+   return this.http.get<string>(`${environment.FLASK_API_URL}/hardware/id`)
+  }
 }
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,4 @@
 export const environment = {
-    FLASK_API_URL:"http://192.168.4.1/api"
+    FLASK_API_URL:"http://192.168.4.1/api",
+    REDIRECT_URL:""
 };
