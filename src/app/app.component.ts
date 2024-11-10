2352d45b9b4efbe0d152f55b772ee5bf79898636
PGaillot <pr.gaillot@gmail.com>
2024-11-10 14:13:50 +0100

    add first lottie.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DoorComponent } from "./components/door/door.component";
+import { AnimationOptions, LottieComponent } from 'ngx-lottie';
+import { AnimationItem } from 'lottie-web';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DoorComponent],
+  imports: [RouterOutlet, LottieComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'toctoc-conect-frontend';
+  title = 'toctoc-conect';
+  animationItem: AnimationItem | undefined;
+
+  options: AnimationOptions = {
+    path: './animations/door_sleep.json',
+    loop: true,
+    autoplay: true
+  };
+
+
+  animationCreated(animationItem: AnimationItem): void {
+    this.animationItem = animationItem;
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,10 +4,15 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 
+import { provideCacheableAnimationLoader, provideLottieOptions } from 'ngx-lottie';
+import player from 'lottie-web';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideHttpClient(),
+    provideLottieOptions({ player: () => player }),
+    provideCacheableAnimationLoader(),
     provideRouter(routes),
   ],
 };
